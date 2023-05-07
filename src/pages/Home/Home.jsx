9e9cfbffa6a4139bc4e791697988710bb4db4279
kishan kumar <rajkishan7870@gmail.com>
2023-05-07 22:55:03 +0530

    footer, sideNav, login, and register total

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
-
+import React from "react";
+import Style from "./Home.module.css";
+import SideNav from "../Slides/SideNav/SideNav";
+import Right from "../Slides/Right/Right";
+import Middle from "../Slides/Middle/Middle";
 import Footer from '../Footer/Footer'
 
-export default function Home() {
+
+export default function Home(){
   return (
+
     <div>
-      <h1>Home</h1>
+    <div className={Style.root}>
+      <div className={Style.left}>
+        <SideNav />
+      </div>
+      <div className={Style.middle}>
+        <Middle/>
+      </div>
+      <div className={Style.right}>
+       <Right/>
+      </div>
+    </div>
+
+    <div>
       <Footer/>
     </div>
-  )
-}
+
+    </div>
+  );
+};
+
